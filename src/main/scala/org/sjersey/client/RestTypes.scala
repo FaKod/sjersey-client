6dc766fe1f4a46d215ddbc550c01c5804d06d90f
FaKod <info@fakod.eu>
2010-12-27 22:12:29 +0100

    added comments and Rest Exception Wrapper function type

--- src/main/scala/org/sjersey/client/RestTypes.scala
+++ src/main/scala/org/sjersey/client/RestTypes.scala
@@ -8,6 +8,18 @@ import com.sun.jersey.api.client.WebResource
  * @author Christopher Schmidt
  */
 private[client] object RestTypes {
+  /**
+   * class WebResource containes class Builder
+   */
   type builderType = WebResource#Builder
+
+  /**
+   * function type of creating a instance of class Builder
+   */
   type BuilderFuncType = (String, RestCallSettings, Boolean) => builderType
+
+  /**
+   * function type of REST exception handler
+   */
+  type ExceptionHandlerType = (Throwable => Unit)
 }
