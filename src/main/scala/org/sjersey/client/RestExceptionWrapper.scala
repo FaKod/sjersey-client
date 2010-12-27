c5b3715cad41801e64035595215b93a7749fc09c
FaKod <info@fakod.eu>
2010-12-27 22:11:28 +0100

    added Resource folder

---
+++ src/main/scala/org/sjersey/client/RestExceptionWrapper.scala
@@ -0,0 +1,11 @@
+package org.sjersey.client
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: christopherschmidt
+ * Date: 27.12.10
+ * Time: 21:00
+ * To change this template use File | Settings | File Templates.
+ */
+
+trait RestExceptionWrapper
