b8fdb40e54ee6300af8a193c09a62e4f346debd8
FaKod <info@fakod.eu>
2010-12-31 16:21:12 +0100

    added class and object to ease access to ClientResponse

--- src/main/scala/org/sjersey/client/RichClientResponse.scala
+++ src/main/scala/org/sjersey/client/RichClientResponse.scala
@@ -1,11 +1,36 @@
 package org.sjersey.client
 
 import com.sun.jersey.api.client.ClientResponse
+import collection.JavaConversions._
+import java.{util => ju}
+import collection.mutable
 
 /**
+ * converts to RichClientResponse
+ *
  * @author Christopher Schmidt
  */
 
-class RichClientResponse(cr:ClientResponse) {
+object RichClientResponse {
+
+  implicit def toRichClientResponse(cr: ClientResponse) = new RichClientResponse(cr)
+
+}
+
+/**
+ * to ease the access to Scala like Collections
+ *
+ * @author Christopher Schmidt
+ */
+class RichClientResponse(cr: ClientResponse) {
+
+  /**
+   * Get the HTTP headers of the response.
+   *
+   * @return the HTTP headers of the response.
+   */
+  def headers:mutable.Map[String, String] = cr.getHeaders.asInstanceOf[ju.Map[String, String]]
+
+  //@TODO implement the rest...
 
 }
